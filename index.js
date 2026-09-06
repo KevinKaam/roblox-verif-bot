eaa3977eeca486c39d0ca5ca7e20d153279fc09d
KevinKaam <113790080+KevinKaam@users.noreply.github.com>
2026-09-06 01:23:24 -0400

    index.js

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 console.log("🔄 Intentando conectar KaamStore BOT a Discord...");
 
 if (!TOKEN) {
-
     console.error("❌ DISCORD_TOKEN no existe.");
-
 } else {
-
-    console.log(
-        "✅ DISCORD_TOKEN detectado. Intentando iniciar sesión..."
-    );
+    console.log("✅ DISCORD_TOKEN detectado. Intentando iniciar sesión...");
 
     client.login(TOKEN)
-
         .then(() => {
-
-            console.log(
-                "✅ Solicitud de conexión a Discord enviada."
-            );
-
+            console.log("✅ Solicitud de conexión a Discord enviada.");
         })
-
-        .catch(error => {
-
-            console.error(
-                "❌ ERROR CRÍTICO AL CONECTAR DISCORD:"
-            );
-
+        .catch((error) => {
+            console.error("❌ ERROR CRÍTICO AL CONECTAR DISCORD:");
             console.error(error);
-
         });
 }
